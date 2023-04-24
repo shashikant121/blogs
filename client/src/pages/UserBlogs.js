4db1937a3464b5eb2e8a6512ef8e68f8872a57e9
unknown <shashikant.prasad49@gmail.com>
2023-04-25 03:49:19 +0530

    First commit

---
+++ client/src/pages/UserBlogs.js
@@ -0,0 +1,50 @@
+import React, { useState, useEffect } from "react";
+import axios from "axios";
+import BlogCard from "../components/BlogCard";
+const UserBlogs = () => {
+  const [blogs, setBlogs] = useState([]);
+  const [loader ,setLoader]=useState(false)
+
+  //get user blogs
+  const getUserBlogs = async () => {
+    try {
+      setLoader(true)
+      const id = localStorage.getItem("userId");
+      const { data } = await axios.get(`/api/v1/blog/user-blog/${id}`);
+      if (data?.success) {
+        setBlogs(data?.userBlog.blogs);
+      }
+      setLoader(false)
+    } catch (error) {
+      setLoader(false)
+      console.log(error);
+    }
+  };
+
+  useEffect(() => {
+    getUserBlogs();
+  }, []);
+  console.log(blogs);
+  return (
+    <div>
+      {loader && loader ? (<><p>Loading................</p></>):(<>{blogs && blogs.length > 0 ? (
+        blogs.map((blog) => (
+          <BlogCard
+            id={blog._id}
+            isUser={true}
+            title={blog.title}
+            description={blog.description}
+            image={blog.image}
+            username={blog.user.username}
+            time={blog.createdAt}
+          />
+        ))
+      ) : (
+        <h1>You Havent Created a blog</h1>
+      )}</>)}
+      
+    </div>
+  );
+};
+
+export default UserBlogs;
